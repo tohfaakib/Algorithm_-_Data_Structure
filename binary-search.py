b0ee3b95e5804d247c4f3c08e372d8bd7ada540d
tohfaakib <tohfaakib@my.smccd.edu>
2018-10-30 23:24:02 +0600

    Binary Search Practice

---
+++ binary-search.py
@@ -0,0 +1,23 @@
+from math import ceil
+
+
+def binary_src(list, item):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = ceil((high + low) / 2)
+        # print(mid)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+list = [1, 3, 5, 7, 9, 10, 20, 23, 56, 60, 100]
+item = int(input("enter search item: "))
+print(item, " is in index: ", binary_src(list, item))
